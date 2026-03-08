c521f6b133575b1a8fa62eeab1d2e1468c20ffdf
Dev-Creator-web <zeeshanmughal1122334400@gmail.com>
2026-03-08 23:01:24 +0500

    initial commit

--- server/seedAdmin.js
+++ server/seedAdmin.js
@@ -9,22 +9,34 @@ async function createAdmin() {
     try {
         await mongoose.connect(process.env.MONGO_URI);
 
-
-        const existingAdmin = await Admin.findOne({ email: 'admin@zumarlawfirm.com' });
-        if (existingAdmin) {
-            console.log('Admin already exists');
-            return process.exit();
+        // First Admin
+        const existingAdmin1 = await Admin.findOne({ email: 'admin@zumarlawfirm.com' });
+        if (!existingAdmin1) {
+            const hashedPassword1 = await bcrypt.hash('zumarlawfirm@7806', 10);
+            const admin1 = new Admin({
+                email: 'admin@zumarlawfirm.com',
+                password: hashedPassword1,
+            });
+            await admin1.save();
+            console.log('First Admin created: admin@zumarlawfirm.com');
+        } else {
+            console.log('First Admin already exists');
         }
 
-        const hashedPassword = await bcrypt.hash('zumarlaw123321', 10);
+        // Second Admin
+        const existingAdmin2 = await Admin.findOne({ email: 'zakonics@gmail.com' });
+        if (!existingAdmin2) {
+            const hashedPassword2 = await bcrypt.hash('zakonics121212', 10);
+            const admin2 = new Admin({
+                email: 'zakonics@gmail.com',
+                password: hashedPassword2,
+            });
+            await admin2.save();
+            console.log('Second Admin created: zakonics@gmail.com');
+        } else {
+            console.log('Second Admin already exists');
+        }
 
-        const admin = new Admin({
-            email: 'admin@zumarlawfirm.com',
-            password: hashedPassword,
-        });
-
-        await admin.save();
-        console.log('Admin created');
         process.exit();
     } catch (error) {
         console.error('Error:', error);
