7a7850df3fbd77c64eb1dcaaad5828b86c904ff0
Dev-Creator-web <zeeshanmughal1122334400@gmail.com>
2026-04-07 22:21:43 +0500

    final

--- client/src/pages/admin/AdminLogin.jsx
+++ client/src/pages/admin/AdminLogin.jsx
@@ -18,7 +18,7 @@ const AdminLogin = () => {
 
     try {
       const res = await axios.post(
-        "https://app.zumarlawfirm.com/auth/login",
+        "https://app.zumarlawfirm.com/admin/login",
         { email, password },
         { withCredentials: true }
       );
